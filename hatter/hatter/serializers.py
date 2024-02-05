0c72763718e3c2458135618cc0d30f636f5d8646
cakemanny <goldingd89@gmail.com>
2024-02-05 23:36:14 +0100

    format

--- hatter/hatter/serializers.py
+++ hatter/hatter/serializers.py
@@ -4,25 +4,26 @@ from hatter.models import HatOrder
 
 serializers.HyperlinkedRelatedField
 
+
 class HatOrderSerializer(serializers.ModelSerializer):
     customer = serializers.SlugRelatedField(
         read_only=True,
-        slug_field='username',
+        slug_field="username",
     )
 
     class Meta:
         model = HatOrder
-        fields = ['customer', 'colour', 'material', 'order_number']
+        fields = ["customer", "colour", "material", "order_number"]
 
     def create(self, validated_data):
-        assert 'request' in self.context, (
+        assert "request" in self.context, (
             "`%s` requires the request in the serializer"
             " context. Add `context={'request': request}` when instantiating "
             "the serializer." % self.__class__.__name__
         )
 
-        request = self.context['request']
-        validated_data['customer_id'] = request.user.id
+        request = self.context["request"]
+        validated_data["customer_id"] = request.user.id
         order = HatOrder(**validated_data)
         order.save()
         return order
--- hatter/hatter/views.py
+++ hatter/hatter/views.py
@@ -2,13 +2,17 @@ from rest_framework.authentication import SessionAuthentication, BasicAuthentica
 from rest_framework.permissions import IsAuthenticated
 
 from rest_framework import status
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.decorators import (
+    api_view,
+    authentication_classes,
+    permission_classes,
+)
 from rest_framework.response import Response
 
 from hatter.serializers import HatOrderSerializer
 
 
-@api_view(['GET', 'POST'])
+@api_view(["GET", "POST"])
 def healthz(request):
     """
     health endpoint for use in k8s etc
@@ -17,14 +21,11 @@ def healthz(request):
     return Response("alles jut, ne!", status=status.HTTP_200_OK)
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 @authentication_classes([SessionAuthentication, BasicAuthentication])
 @permission_classes([IsAuthenticated])
 def order_hat(request) -> Response:
-    serializer = HatOrderSerializer(
-        data=request.data,
-        context={'request': request}
-    )
+    serializer = HatOrderSerializer(data=request.data, context={"request": request})
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
